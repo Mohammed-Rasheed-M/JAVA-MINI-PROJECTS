b90c423be976710d37b955441a19f2f4771408c2
Mohammed-Rasheed-M <71689240+Mohammed-Rasheed-M@users.noreply.github.com>
2021-11-06 13:19:32 +0530

    Sudoku Solver using JAVA

---
+++ Soduko-Solver/Sodoko.java
@@ -0,0 +1,97 @@
+class Sodoko {
+    private static final int GRID_SIZE = 9;
+
+    public static void main(String[] args) {
+        System.out.println("********************************************************");
+        System.out.println("This is a Soduko Solver !!");
+        System.out.println("This is program is created by Rasheed M ©");
+        System.out.println("********************************************************");
+        System.out.println();
+
+        int[][] board = { { 7, 0, 2, 0, 5, 0, 6, 0, 0 }, { 0, 0, 0, 0, 0, 3, 0, 0, 0 }, { 1, 0, 0, 0, 0, 9, 5, 0, 0 },
+                { 8, 0, 0, 0, 0, 0, 0, 9, 0 }, { 0, 4, 3, 0, 0, 0, 7, 5, 0 }, { 0, 9, 0, 0, 0, 0, 0, 0, 8 },
+                { 0, 0, 9, 7, 0, 0, 0, 0, 5 }, { 0, 0, 0, 2, 0, 0, 0, 0, 0 }, { 0, 0, 7, 0, 4, 0, 2, 0, 3 } };
+        printBoard(board);
+        
+        if (solveBoard(board)) {
+            System.out.println("Solved successfully!");
+        } else {
+            System.out.println("Unsolvable board ");
+        }
+
+        printBoard(board);
+
+    }
+    private static void printBoard(int[][] board) {
+        for (int row = 0; row < GRID_SIZE; row++) {
+            if (row % 3 == 0 && row != 0) {
+                System.out.println("-----------");
+            }
+            for (int column = 0; column < GRID_SIZE; column++) {
+                if (column % 3 == 0 && column != 0) {
+                    System.out.print("|");
+                }
+                System.out.print(board[row][column]);
+            }
+            System.out.println();
+        }
+    }
+
+    private static boolean isNumberInRow(int[][] board, int number, int row) {
+        for (int i = 0; i < GRID_SIZE; i++) {
+            if (board[row][i] == number) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean isNumberInCol(int[][] board, int number, int col) {
+        for (int i = 0; i < GRID_SIZE; i++) {
+            if (board[i][col] == number) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean isNumberInBox(int[][] board, int number, int col, int row) {
+        int localBoxRow = row - row % 3;
+        int localBoxCol = col - col % 3;
+        for (int i = localBoxRow; i < localBoxRow + 3; i++) {
+            for (int j = localBoxCol; j < localBoxCol + 3; j++) {
+                if (board[i][j] == number) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private static boolean isValidPlacement(int[][] board, int number, int row, int col) {
+        return !isNumberInRow(board, number, row) && !isNumberInCol(board, number, col)
+                && !isNumberInBox(board, number, row, col);
+    }
+
+    private static boolean solveBoard(int[][] board) {
+        for (int row = 0; row < GRID_SIZE; row++) {
+            for (int column = 0; column < GRID_SIZE; column++) {
+                if (board[row][column] == 0) {
+                    for (int numberToTry = 1; numberToTry <= GRID_SIZE; numberToTry++) {
+                        if (isValidPlacement(board, numberToTry, row, column)) {
+                            board[row][column] = numberToTry;
+
+                            if (solveBoard(board)) {
+                                return true;
+                            } else {
+                                board[row][column] = 0;
+                            }
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
